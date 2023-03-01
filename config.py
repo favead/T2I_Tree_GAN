7987bc87f5830495b2510534ff5b9555b9f25ca0
favead <diablo3_monk@mail.ru>
2023-03-01 12:46:48 +0300

    Add template for configuration of model params and training process

---
+++ config.py
@@ -0,0 +1,19 @@
+from typing import Any, List, Tuple, Callable, Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    """
+    Configure when developing new model
+    """
+    batch_size: int
+    epochs: int
+    gamma: float
+    lr: float
+    betas: Tuple[float]
+    train_file: str
+    val_file: str
+    weight_dir: Tuple[str, str]
+    test_data_paths: Tuple[str, str]
+    train_data_path: Tuple[str, str]
