f9888bc155b5c53dea41e37c876072f631ae77d4
favead <diablo3_monk@mail.ru>
2023-03-01 12:41:44 +0300

    Create a template file for neural network

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn, Tensor
+
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super(Model, self).__init__()
+        return None
+    
+    def forward(self, x: Tensor) -> Tensor:
+        pass
