4c9cd1b4d6c7fc76a709a0434671c04693f56d11
favead <diablo3_monk@mail.ru>
2023-03-04 15:12:41 +0300

    Add average meter class

---
+++ metrics.py
@@ -0,0 +1,19 @@
+import torch
+from torch import Tensor
+
+
+class AverageMeter:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val: float, n: int = 1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
