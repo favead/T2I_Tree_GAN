2bdb3ee557a147d2aefee4966d4480bee1b6fb0f
favead <diablo3_monk@mail.ru>
2023-03-07 19:15:50 +0300

    (1.1.0) Change dataset function

--- dataproc.py
+++ dataproc.py
@@ -1,4 +1,5 @@
 from typing import Callable, Tuple, List
+import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 import h5py
@@ -6,29 +7,20 @@ import numpy as np
 
 
 class SRFolderDataset(Dataset):
-    def __init__(self, files: List[str], crop_times: int, scale: int,
-                 crop_area: int, read_image: Callable, resize_image: Callable,
-                 crop_image: Callable, ts: Callable = None) -> None:
+    def __init__(self, files: List[str], read_image: Callable, ts: Callable = None) -> None:
         super(SRFolderDataset, self).__init__()
         self.files = files
         self.ts = ts
-        self.crop_times = crop_times
-        self.scale = scale
-        self.crop_area = crop_area
         self.read_image = read_image
-        self.crop_image = crop_image
-        self.resize_image = resize_image
         return None
         
     def __getitem__(self, indx: int) -> Tuple[Tensor, Tensor]:
-        hr = self.read_image(self.files[indx // self.crop_times])
-        hr_crop = self.crop_image(hr, self.crop_area)
-        inter = np.random.randint(0, 3)
-        lr_crop = self.resize_image(hr_crop, scale=self.scale, is_up=False, typ=inter)
+        y = self.read_image(self.files[indx])
+        x = torch.rand((y.shape)) * 255
         for t in self.ts:
-            hr_crop = t(hr_crop)
-            lr_crop = t(lr_crop)
-        return lr_crop, hr_crop
+            y = t(y)
+            x = t(x)
+        return x, y
         
     def __len__(self) -> int:
         return len(self.files) * self.crop_times
