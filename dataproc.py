4d6eddb5420da4e547e412564ca24a8064454262
favead <diablo3_monk@mail.ru>
2023-03-07 19:19:35 +0300

    (1.1.0) Some changes in Folder dataset

--- dataproc.py
+++ dataproc.py
@@ -23,21 +23,4 @@ class SRFolderDataset(Dataset):
         return x, y
         
     def __len__(self) -> int:
-        return len(self.files) * self.crop_times
-
-        
-class SRTestDataset(Dataset):
-    def __init__(self, files: List[Tuple[str, str]], x_T: List[Callable], get_data: Callable) -> None:
-        super(SRTestDataset, self).__init__()
-        self.files = files
-        self.x_T = x_T
-        self.get_data = get_data
-        
-    def __getitem__(self, indx: int) -> Tuple[Tensor, Tensor]:
-        x_path, y_path = self.files[indx]
-        x, y = map(self.get_data, [x_path, y_path])
-        x = self.x_T(x)
-        return x, y
-    
-    def __len__(self) -> int:
         return len(self.files)
