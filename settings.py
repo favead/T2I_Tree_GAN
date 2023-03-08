fd48c36cad16cc68540e782d3920d27a1b4e473e
favead <diablo3_monk@mail.ru>
2023-03-08 21:28:58 +0300

    Version 1.2.0

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 config = {
-    "version": "1.1.0",
-    "batch_size": 512,
+    "version": "1.2.0",
+    "batch_size": 256,
     "epochs": 100,
     "lr_gen": 2e-4,
-    "lr_disc": 3e-4,
-    "betas_disc": (0.9, 0.3),
-    "betas_gen": (0.5, 0.35),
+    "lr_disc": 1e-5,
+    "betas_disc": (0.85, 0.4),
+    "betas_gen": (0.4, 0.35),
     "checkpoint": 5,
     "weight_checkpoint": 15,
-    "weight_dir": ("trg1_disc.pt", "trg1_gen.pt"),
-    "project_name": "TreeGAN_1.1.0"
+    "weight_dir": ("trg1_2_disc.pt", "trg1_2_gen.pt"),
+    "project_name": "TreeGAN_1.2.0"
 }
