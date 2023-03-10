27edda10947103ef2c547830928976d0f7b2f53e
favead <diablo3_monk@mail.ru>
2023-03-10 23:20:13 +0300

    Update lr's

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 config = {
-    "version": "2.0.0",
+    "version": "2.0.1",
     "batch_size": 256,
-    "epochs": 30,
-    "lr_gen": 2e-4,
-    "lr_disc": 2e-4,
+    "epochs": 60,
+    "lr_gen": 3e-5,
+    "lr_disc": 3e-5,
     "betas_disc": (0.5, 0.999),
     "betas_gen": (0.5, 0.999),
     "checkpoint": 1,
     "weight_checkpoint": 15,
     "weight_dir": ("trg2_0_0_disc.pt", "trg2_0_0_gen.pt"),
-    "project_name": "TreeGAN_2.0.0"
+    "project_name": "TreeGAN_2.0.1"
 }
