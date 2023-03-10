820e5e6fc85a8293d7ec4b5356b11aa2025ea81c
favead <diablo3_monk@mail.ru>
2023-03-10 22:27:11 +0300

    Version 2.0.0

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ from torch import nn, Tensor
 
 
 class GenTransposeBlock(nn.Module):
-    def __init__(self, in_c: int, out_c: int, stride: int, padding: int, is_last: bool) -> None:
+    def __init__(self, in_c: int, out_c: int, stride: int, padding: int,
+                  is_last: bool = False) -> None:
         super(GenTransposeBlock, self).__init__()
         self.block = nn.Sequential()
         self.block.add_module("ConvTranspose", nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=stride,
@@ -31,7 +32,7 @@ class Generator(nn.Module):
         self.tconv2 = GenTransposeBlock(1024, 512, 2, 1) # (BS, 512, 2, 2)
         self.tconv3 = GenTransposeBlock(512, 128, 2, 1) # (BS, 128, 4, 4)
         self.tconv4 = GenTransposeBlock(128, 32, 2, 1) # (BS, 32, 8, 8)
-        self.tconv5 = GenTransposeBlock(32, 3, 2, 1, True) # (BS, 3, 16, 16)
+        self.tconv5 = GenTransposeBlock(32, 3, 2, 1, is_last=True) # (BS, 3, 16, 16)
         return None
     
     def forward(self, x: Tensor) -> Tensor:
@@ -45,7 +46,7 @@ class Generator(nn.Module):
 
 class DiscBlock(nn.Module):
     def __init__(self, in_c: int, out_c: int, stride: int, padding: int,
-                 is_last: bool) -> None:
+                 is_last: bool = False) -> None:
         super(DiscBlock, self).__init__()
         self.block = nn.Sequential()
         self.block.add_module("Conv", nn.Conv2d(in_c, out_c, kernel_size=2, stride=stride,
@@ -68,7 +69,7 @@ class Discriminator(nn.Module):
         self.conv1 = DiscBlock(16, 8, 2, 1)
         self.conv2 = DiscBlock(8, 4, 2, 1)
         self.conv3 = DiscBlock(4, 2, 2, 1)
-        self.conv4 = DiscBlock(2, 1, 2, 1, True)
+        self.conv4 = DiscBlock(2, 1, 2, 1, is_last=True)
         return None
     
     def forward(self, x: Tensor) -> Tensor:
