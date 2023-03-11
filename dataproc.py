2873042b60f0164f634662f540eb85e1ce0a1bc3
favead <diablo3_monk@mail.ru>
2023-03-11 18:00:15 +0300

    Version 2.4.0

--- dataproc.py
+++ dataproc.py
@@ -3,23 +3,25 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 import numpy as np
+import h5py
 
 
-class SRFolderDataset(Dataset):
-    def __init__(self, files: List[str], read_image: Callable, ts: Callable = None) -> None:
+class SRFileDataset(Dataset):
+    def __init__(self, filename: str, ts: Callable = None) -> None:
         super(SRFolderDataset, self).__init__()
-        self.files = files
+        self.filename = filename
         self.ts = ts
-        self.read_image = read_image
         return None
-        
+
     def __getitem__(self, indx: int) -> Tuple[Tensor, Tensor]:
-        y = self.read_image(self.files[indx])
-        assert y.shape == (16, 16, 3)
         x = torch.randn(100, 1, 1)
-        for t in self.ts:
-            y = t(y)
-        return x, y
-        
+        with h5py.File(self.filename, 'r') as f:
+            y = f['y'][indx]
+            assert y.shape == (16, 16, 3)
+            for t in self.ts:
+                y = t(y)
+            return x, y
+
     def __len__(self) -> int:
-        return len(self.files)
+        with h5py.File(self.filename, 'r') as f:
+            return len(f['y'])
--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 config = {
-    "version": "2.3.1",
+    "version": "2.4.0",
     "batch_size": 256,
-    "epochs": 60,
+    "epochs": 41,
     "lr_gen": 2e-4,
     "lr_disc": 2e-4,
     "betas_disc": (0.5, 0.999),
     "betas_gen": (0.5, 0.999),
     "checkpoint": 1,
-    "weight_checkpoint": 15,
-    "weight_dir": ("trg2_0_0_disc.pt", "trg2_0_0_gen.pt"),
-    "project_name": "TreeGAN_2.3.1"
+    "weight_checkpoint": 10,
+    "weight_dir": ("trg2_4_0_disc.pt", "trg2_4_0_gen.pt"),
+    "project_name": "TreeGAN_2.4.0"
 }
