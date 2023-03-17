ff5554c734a4355f04953162095c60f46659d8c9
favead <diablo3_monk@mail.ru>
2023-03-17 16:39:24 +0300

    Change embedding load function

--- dataproc.py
+++ dataproc.py
@@ -14,19 +14,19 @@ class T2IFolderDataset(Dataset):
         self.read_image = read_image
         self.ts = ts
 
-    def get_wrong_caption(self, actual_index: int) -> str:
+    def get_random_embedding(self, actual_index: int) -> str:
         index = torch.randint(0, len(self.files), (1,)).item()
-        if index == actual_index:
-            return self.get_wrong_caption(actual_index)
-        rnd_caption = self.read_data(self.files[index])['caption']
-        return rnd_caption
+        while index == actual_index:
+            index = torch.randint(0, len(self.files), (1,)).item()
+        rnd_emb = self.get_embedding(self.files[index])
+        return rnd_emb
 
     def __getitem__(self, index: int) -> Tuple[Tensor, Tensor, Tensor]:
         sample = self.read_data(self.files[index])
-        sent_emb = self.get_embedding(sample['caption'])
+        sent_emb = self.get_embedding(sample['img_path'])
         y = self.read_image(sample['img_path'])
-        wrong_cap = self.get_wrong_caption(index)
-        wrong_emb = 0.5 * self.get_embedding(wrong_cap) + 0.5 * sent_emb
+        rnd_emb = self.get_random_embedding(index)
+        wrong_emb = 0.5 * rnd_emb + 0.5 * sent_emb
         sent_emb = sent_emb.view(768, 1, 1)
         wrong_emb = wrong_emb.view(768, 1, 1)
         for t in self.ts:
