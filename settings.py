0bf465931891ec63c27724e11ab675b8c24f0208
favead <diablo3_monk@mail.ru>
2023-03-19 20:18:13 +0300

    Version 1.0.7

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 config = {
-    "version": "1.0.6",
-    "batch_size": 256,
+    "version": "1.0.7",
+    "batch_size": 512,
     "epochs": 30,
-    "lr_gen": 1e-5,
-    "lr_disc": 1e-5,
+    "lr_gen": 1e-6,
+    "lr_disc": 1e-6,
     "betas_disc": (0.5, 0.999),
     "betas_gen": (0.5, 0.999),
     "checkpoint": 1,
@@ -11,5 +11,5 @@ config = {
     'gen_iter': 2,
     'disc_iter': 1,
     "weight_dir": ("t2itree_disc.pt", "t2itree_gen.pt"),
-    "project_name": "T2ITreeGAN_1.0.6"
+    "project_name": "T2ITreeGAN_1.0.7"
 }
